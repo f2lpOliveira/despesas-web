f87335704aa1addf22675ffd956cc95f13efc7ac
f2lpOliveira <contato@fellipefreire.com.br>
2024-04-11 15:50:20 -0300

    refactor: Mapeamento de Servlet através de anotação com urlPatterns.

--- src/main/java/br/com/fellipefreire/PrimeiraServlet.java
+++ src/main/java/br/com/fellipefreire/PrimeiraServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet("/segundaServlet")
+@WebServlet(urlPatterns = {"/primeiraServlet", "xpto"})
 public class PrimeiraServlet extends HttpServlet {
 
   @Override
